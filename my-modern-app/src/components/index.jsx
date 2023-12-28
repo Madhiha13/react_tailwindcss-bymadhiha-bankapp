9fecdd6819f2938fddb744de558e8db2392f90c0
Madhiha13 <madhihakhanamsk13@gmail.com>
2023-12-29 03:27:18 +0530

    first commit

---
+++ my-modern-app/src/components/index.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Clients from "./Clients"
+import Hero from "./Hero";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import CTA from "./CTA";
+import Footer from "./Footer";
+
+export {
+    Navbar,
+    Clients,
+    Hero,
+    Billing,
+    CardDeal,
+    Business,
+    Stats,
+    Testimonials,
+    CTA,
+    Footer,
+};
